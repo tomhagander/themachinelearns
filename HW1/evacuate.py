9ab7daa034c819254c4ae0cc56c81a3b55243ede
Tom Hagander <tom.hagander@gmail.com>
2022-10-10 14:08:44 -0700

    plotting halfway done

--- HW1/evacuate.py
+++ HW1/evacuate.py
@@ -1,10 +1,19 @@
 import csv
+import matplotlib.pyplot as plt
 
 points = []
+xs = []
+ys = []
 # importing csv
 with open('HW1data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         points.append((float(row[0]), float(row[1])))
+        xs.append(float(row[0]))
+        ys.append(float(row[1]))
 
 print(points)
+
+plt.plot(xs, ys, 'o')
+plt.grid()
+plt.show()
